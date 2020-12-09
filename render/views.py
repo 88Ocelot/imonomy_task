f17edb474bd390e6145a3412a744170362058c78
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-12-09 17:59:16 +0200

    added filtering

--- render/views.py
+++ render/views.py
@@ -3,5 +3,11 @@ from render.models import ApiData
 
 
 def render_apidata(request):
+    cname_filter = request.GET.get('client_name', None)
+    date_filter = request.GET.get('date', None)
     data = ApiData.objects.all()
+    if cname_filter:
+        data = data.filter(client_name=cname_filter)
+    if cname_filter:
+        data = data.filter(date=date_filter)
     return render(request, 'index.html', {'data': data})
