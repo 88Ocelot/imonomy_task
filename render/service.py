23f80925e74c6369e4d3b17a36f0b00ad196999e
88Ocelot <stas.mitrofanov1337@gmail.com>
2020-12-09 16:58:43 +0200

    added service for retriving data

---
+++ render/service.py
@@ -0,0 +1,29 @@
+import json
+import urllib.request
+import datetime
+from .models import ApiData
+
+API_KEY = '4e923a83-8b31-4c28-ac3d-f6cb994a60af'
+API_URL = 'http://publishers.imonomy.com/api/reports{query_params}'
+QUERY_PARAMS = '?key={api_key}&type_name=dsp&format=json&date_from={date_from}&date_to={date_to}'
+
+
+def _get_api_url(date_from=None, date_to=None):
+    if date_from is None or date_to is None:
+        date_to = datetime.date.today().strftime('%Y-%m-%d')
+        date_from = (datetime.date.today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
+    return API_URL.format(query_params=QUERY_PARAMS.format(api_key=API_KEY,
+                                                           date_from=date_from,
+                                                           date_to=date_to))
+
+
+def _get_api_response(api_url):
+    with urllib.request.urlopen(api_url) as url:
+        data = json.loads(url.read().decode())
+        print(data)
+    return data
+
+
+if __name__ == '__main__':
+    response = _get_api_response(_get_api_url())
+
